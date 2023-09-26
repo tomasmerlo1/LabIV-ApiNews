075aeb60f1906c84e47ca93c85e38f2ed5bbe440
tomasmerlo1 <tomasmerlo2018@gmail.com>
2023-09-26 20:11:47 -0300

    bitcoin

--- controllers/new.js
+++ controllers/new.js
@@ -3,42 +3,62 @@ const { request, response } = require('express')
 
 const key = 'bc1faf88178f4e458017474fed0bd5bb';
 
-const getNoticias = (req = request, res = response) => {
-    const {id} = req.params;
-    console.log(id)
-    res.status(200).json({name: `Noticia numero:${id}`});
-}
 
 const getNoticiasArgentinas = (req = request, res = response) => {
     console.log(req.params);
     const { name } = req.params;
 
+    axios.get(`https://newsapi.org/v2/top-headlines?country=ar&apiKey=${key}`)
+        .then(({ status, data, statusText }) => {
+            // handle success
+            console.log({ status, data, statusText });
+            res.status(200).json({
+                status,
+                data,
+                statusText,
+                name
+            });
+        })
+        .catch((error) => {
+            // handle error
+            console.log(error);
+            res.status(400).json({
+                status:400,
+                msg: 'Error inesperado'
+            });
+        });
 
 
-axios.get(`https://newsapi.org/v2/top-headlines?country=ar&apiKey=${key}`)
-    .then(({ status, data, statusText }) => {
-        // handle success
-        console.log({ status, data, statusText });
-        res.status(200).json({
-            status,
-            data,
-            statusText,
-            name
+}
+
+const getPalabraClave = (req = request, res = response) => {
+    console.log(req.params);
+    const { q } = req.query;
+
+    axios.get(`https://newsapi.org/v2/everything?country=ar&apiKey=${key}&q=${q}`)
+        .then(({ status, data, statusText }) => {
+            // handle success
+            console.log({ status, data, statusText });
+            res.status(200).json({
+                status,
+                data,
+                statusText,
+                q,
+            });
+        })
+        .catch((error) => {
+            // handle error
+            console.log(error);
+            res.status(400).json({
+                status:400,
+                msg: 'Error inesperado'
+            });
         });
-    })
-    .catch((error)=>{
-        // handle error
-        console.log(error);
-        res.status(400).json({
-            status:400,
-            msg: 'Error inesperado'
-        });
-    });
 
 
 }
 
 module.exports = {
-    getNoticias,
-    getNoticiasArgentinas
+    getNoticiasArgentinas, 
+    getPalabraClave
 };
--- routes/news.js
+++ routes/news.js
@@ -1,12 +1,11 @@
 const { Router } = require('express')
 
-const { getNoticias, getNoticiasArgentinas } = require('../controllers/new')
+const { getNoticiasArgentinas, getPalabraClave } = require('../controllers/new')
 
 const rutas = Router();
 
-rutas.get('/noticias/:id', getNoticias);
-
-rutas.get('/noticiasArgentinas', getNoticiasArgentinas);
+rutas.get('/noticiasargentinas', getNoticiasArgentinas);
+rutas.get('/noticiaspalabraclave/:q', getPalabraClave);
 
 
 
