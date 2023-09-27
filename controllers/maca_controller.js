a96a9292d0f07561abaed5347e7d744f0431f7d0
Macarena Huarte <113394641+Macahuarte09@users.noreply.github.com>
2023-09-27 17:35:37 -0300

    commit final

--- controllers/maca_controller.js
+++ controllers/maca_controller.js
@@ -6,3 +6,52 @@ const key = process.env.API_KEY;
 if(key == undefined){
     throw new Error("La api key no puede ser")
 }
+const getComerciales = (req = request, res = response) => {
+    console.log(req.params);
+    const {categoria} = req.params; 
+
+    axios.get (`https://newsapi.org/v2/top-headlines/sources?category=${categoria}apiKey=${key}`)
+        .then(({ status, data, statusText }) => {
+            console.log({ status, data, statusText });
+            res.status(200).json({
+                status,
+                data,
+                statusText,
+                categoria,
+            });
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(400).json({
+                status:400,
+                msg: 'Error inesperado'
+            });
+        });
+    }
+    const getNoticiasBBC_News = (req = request, res = response) => {
+        console.log(req.params);
+        const {source} = req.params;
+
+        axios.get (`https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${key}`)
+            .then(({ status, data, statusText }) => {
+                console.log({ status, data, statusText });
+                res.status(200).json({
+                    status,
+                    data,
+                    statusText,
+                    source,
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+                    res.status(400).json({
+                        status:400,
+                        msg: 'Error inesperado'
+                    });
+            });
+        }
+
+module.exports = {
+    getComerciales,
+    getNoticiasBBC_News
+};
